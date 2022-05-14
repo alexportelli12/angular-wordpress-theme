90e7ddfe47814924e701a547efb7dbbf43ac11b1
Alex <alexportelli12@gmail.com>
2022-05-14 16:09:39 +0200

    feat: remove commented code

--- functions.php
+++ functions.php
@@ -6,11 +6,3 @@
 
 // Custom Post Types
 require_once get_template_directory() . '/cpt/classes.php';
-
-// *******************************************************
-// FIGURE OUT WHERE IT WOULD BE BEST TO ADD THE BELOW CODE
-// *******************************************************
-// register_rest_field( 'page', 'metadata', array(
-//     'get_callback' => function ( $data ) {
-//         return get_post_meta( $data['id'], '', '' );
-//     }, ));
